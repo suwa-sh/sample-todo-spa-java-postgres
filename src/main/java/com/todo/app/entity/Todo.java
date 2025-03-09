301297c323b5d70c547c9a2b419f51b55a77197d
Shinichi Suwa <suwash01@gmail.com>
2025-03-10 07:20:10 +0900

    chore: 単純な環境でビルドできるようにする

--- src/main/java/com/todo/app/entity/Todo.java
+++ src/main/java/com/todo/app/entity/Todo.java
@@ -1,17 +1,44 @@
 package com.todo.app.entity;
 
-import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
-
-@Data
-@Getter
-@Setter
 public class Todo {
-
 
 	private long id;
 	private String title;
 	private int done_flg;
 	private String time_limit;
+
+	public Todo() {
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public void setDone_flg(int done_flg) {
+		this.done_flg = done_flg;
+	}
+	public void setTime_limit(String time_limit) {
+		this.time_limit = time_limit;
+	}
+
+	public long getId() {
+		return id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public int getDone_flg() {
+		return done_flg;
+	}
+	public String getTime_limit() {
+		return time_limit;
+	}
+
+	@Override
+	public String toString() {
+		// json形式で出力
+		return "{\"id\":" + id + ", \"title\":\"" + title + "\", \"done_flg\":" + done_flg + ", \"time_limit\":\"" + time_limit + "\"}";
+	}
 }
